d1e67bb24766813df780b5c2135d2cc75051800b
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-03-04 03:49:22 +0530

    initial commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,7 @@
+const signup = async (req, res) => {
+  res.json({
+    message: "signup successful",
+  });
+};
+
+export { signup };
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import movieRoutes from "./routes/movieRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import { config } from "dotenv";
+import { connectDB, disconnectDB } from "./config/db.js";
+
+config();
+connectDB();
+
+const app = express();
+const PORT = 5001;
+
+app.use("/auth", authRoutes);
+app.use("/movies", movieRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught Exception: ${err.message}`);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT received, shutting down gracefully...");
+  await disconnectDB();
+  process.exit(0);
+});
